d99afa1fc59785958b6e7ae9ac7e78ae17f419a3
Chris Hallberg <crhallberg@gmail.com>
2015-12-05 19:46:18 -0500

    Correct lines isolated.

--- aurora_8107.js
+++ aurora_8107.js
@@ -4,7 +4,10 @@ function makePrediction() {
   // Download forecast
   request.get('http://services.swpc.noaa.gov/text/3-day-forecast.txt', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body);
+      var lines = body.split('\n');
+      console.log(lines[8]);
+      console.log(lines[14]);
+      console.log(lines[20]);
       // Continue with your processing here.
     }
   });
